1e8abc5633056bbec6ed5604409542d117428a8e
Justin Reynard <framerate@gmail.com>
2018-08-30 14:13:53 -0700

    Cleanup for prod build

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -12,16 +12,29 @@ module.exports = {
                 }
             },
             {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
+                test: /\.scss$/,
+                use: [
+                    // "style-loader", // creates style nodes from JS strings
+                    'css-loader' // translates CSS into CommonJS
+                    // "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
             },
             {
-                test: /\.(png|svg|jpg|gif)$/,
+                test: /\.(jpe?g|png|gif|svg)$/i,
                 use: [
-                    'file-loader'
+                    'url-loader?limit=10000',
+                    'img-loader'
+                ]
+            },
+            {
+                test: /\.ttf$/,
+                use: [
+                    {
+                        loader: 'ttf-loader',
+                        options: {
+                            name: './fonts/[hash].[ext]'
+                        }
+                    }
                 ]
             }
         ]
--- webpack.config.development.js
+++ webpack.config.development.js
@@ -28,42 +28,4 @@ config.plugins.push(
     )
 )
 
-config.module = {
-    rules: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
-            }
-        },
-        {
-            test: /\.scss$/,
-            use: [
-                //"style-loader", // creates style nodes from JS strings
-                "css-loader", // translates CSS into CommonJS
-                //"sass-loader" // compiles Sass to CSS, using Node Sass by default
-            ]
-        },
-        {
-            test: /\.(jpe?g|png|gif|svg)$/i,
-            use: [
-                'url-loader?limit=10000',
-                'img-loader'
-            ]
-        },
-        {
-            test: /\.ttf$/,
-            use: [
-                {
-                    loader: 'ttf-loader',
-                    options: {
-                        name: './fonts/[hash].[ext]',
-                    },
-                },
-            ]
-        }
-    ]
-};
-
 module.exports = config
