43a39abfbc5cbd95278eb439f58e54518dcb59d8
Justin Reynard <framerate@gmail.com>
2020-01-05 16:43:30 -0800

    /beta -> /code and /beta is new static registration site

--- deployToS3Prod.sh
+++ deployToS3Prod.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # copy the assets folder to the right place and set headers/permissions
-aws s3 sync ./dist s3://doomtroopergame.com/beta \
+aws s3 sync ./dist s3://doomtroopergame.com/code \
 	--region us-west-1 \
 	--grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers \
 
 # invalidate the cloudfront cache
 aws cloudfront create-invalidation --distribution-id E1G3NK2TUN5MU1 \
-  --paths /beta/*
+  --paths /code/*
